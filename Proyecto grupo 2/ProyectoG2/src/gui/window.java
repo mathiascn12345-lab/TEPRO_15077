d5b20509042e61a2f44d67b680ccc51f9e45880c
Fabrizio Andre Eguizagal Rabanal <N00414656@upn.pe>
2026-04-12 22:13:18 -0500

    Modificacion de código

--- Proyecto grupo 2/ProyectoG2/src/gui/window.java
+++ Proyecto grupo 2/ProyectoG2/src/gui/window.java
@@ -174,21 +174,16 @@ public class window extends JFrame implements ActionListener {
 	}
 	protected void do_btnListar_actionPerformed(ActionEvent e) {
 		txtarea.setText("");
-		txtarea.append("Código\tNombre\tCategoría\tPrecio\tStock");
+		txtarea.append("CÓDIGO\tNOMBRE\t\tCATEGORÍA\tPRECIO\tSTOCK");
 		for(int i=0; i<ap.Tamaño();i++) {
 			txtarea.append("\n"+ap.Obtener(i).getCodigo()+"\t"+ap.Obtener(i).getNombre()+"\t\t"+ap.Obtener(i).getCategoria()+"\t"+ap.Obtener(i).getPrecio()+"\t"+ap.Obtener(i).getStock());
 		}
 	}
-	protected void do_btnBuscar_actionPerformed(ActionEvent e) {
+	protected void do_btnBuscar_actionPerformed(ActionEvent e) { // Busca
 		String Nom = txtnom.getText();
 		Producto P = ap.Buscar(Nom);
 		if (P != null) {
-			JOptionPane.showMessageDialog(this,
-					"Codigo: "+P.getCodigo()+"\n"+
-					"Nombre: "+P.getNombre()+"\n"+
-					"Categoría: "+P.getCategoria()+"\n"+
-					"Precio: "+P.getPrecio()+"\n"+
-					"Stock: "+P.getStock());
+			JOptionPane.showMessageDialog(this,"Codigo: "+P.getCodigo()+"\n"+"Nombre: "+P.getNombre()+"\n"+"Categoría: "+P.getCategoria()+"\n"+"Precio: "+P.getPrecio()+"\n"+"Stock: "+P.getStock());
 		}
 		else JOptionPane.showInputDialog(this, "Producto no encontrado");
 	}
